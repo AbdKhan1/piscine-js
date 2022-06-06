a570b09cecaf0993ff6e4e67649f11b3cb5d9d1d
gymlad <abdalraheemkhan@icloud.com>
2022-06-06 15:26:23 +0100

    take2

--- dog-years.js
+++ dog-years.js
@@ -1,20 +1,36 @@
 function dogYears(planet,age){
     const sec = 31557600 
     if (planet == "earth"){
-        return age; 
+        dogAge = age/sec;
+        dogAge = Math.round( dogAge * 100 + Number.EPSILON ) / 100;
+        return dogAge
     }else if (planet=="mercury"){
-        return (age*0.2408467)/sec;
+        dogAge = (age*0.2408467)/sec;
+        dogAge.toFixed(2)
+        return dogAge
     }else if (planet=="venus"){
-        return (age*0.61519726)/sec;
+         dogAge = (age*0.61519726)/sec;
+         dogAge.toFixed(2)
+         return dogAge
     }else if (planet=="mars"){
-        return (age*1.8808158)/sec;
+        dogAge = (age*1.8808158)/sec;
+        dogAge.toFixed(2);
+        return dogAge
     }else if (planet=="jupiter"){
-        return (age*11.862615)/sec;
+        dogAge= (age*11.862615)/sec;
+        dogAge.toFixed(2);
+        return dogAge
     }else if (planet=="saturn"){
-        return (age*29.447498)/sec;
+        dogAge = (age*29.447498)/sec;
+        dogAge.toFixed(2);
+        return dogAge
     }else if (planet=="uranus"){
-        return (age*84.016846)/sec;
+        dogAge = (age*84.016846)/sec;
+        dogAge.toFixed(2);
+        return dogAge
     }else if (planet=="neptune"){
-        return (age*164.79132)/sec;
+        dogAge= (age*164.79132)/sec;
+        dogAge.toFixed(2);
+        return dogAge
     }
 }

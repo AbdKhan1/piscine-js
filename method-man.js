43a2c51caf911273db150444908d1c24e42d6b1d
gymlad <abdalraheemkhan@icloud.com>
2022-06-06 16:44:33 +0100

    take1

---
+++ method-man.js
@@ -0,0 +1,27 @@
+function words(str){
+    str = text.split(" ");
+    return str 
+}
+
+function sentence(arr){
+    arr = arr.join(' ');
+    return arr 
+}
+
+function yell(str){
+   str = str.toUpperCase()
+    return str 
+}
+
+function whisper(str){
+    str = words(str)
+    str = str.unshift("*")
+    str = str.push("*")
+    str = sentece(str)
+    str = str.toLowerCase()
+    return str 
+}
+
+function capitalize(str){
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}

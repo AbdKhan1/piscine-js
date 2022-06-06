f75c6107fd584465d26800a9d9c8a08d98247d6f
gymlad <abdalraheemkhan@icloud.com>
2022-06-06 12:35:53 +0100

    take2

--- declarations.js
+++ declarations.js
@@ -1,6 +1,6 @@
 const escapeStr = "\`\\\\/\"\'";
 const arr = [4, '2'];
-Object.freeze(arr)
+
 
 const obj ={
 str :"abc",
@@ -8,14 +8,16 @@ num : 123,
 bool : true, 
 undef : undefined,
 };
-Object.freeze(obj);
+
 
 const nested = {
-    arr2: [4,undefined,'2'],
-    obj2:{
+    arr: [4,undefined,'2'],
+    obj:{
         str:"xyz",
         num:456,
         bool: false,
     }
 }
-Object.freeze(nested)
+Object.freeze(nested);
+Object.freeze(nested.arr)
+Object.freeze(nested.obj)

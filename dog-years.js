07f9b3b810f790bd5b523a451bc30dbea4b7c328
gymlad <abdalraheemkhan@icloud.com>
2022-06-06 16:15:32 +0100

    take7

--- dog-years.js
+++ dog-years.js
@@ -1,28 +1,17 @@
-function dogYears(planet,age){
-    const sec = 31557600 
-    if (planet == "earth"){
-        let dogAge = age*7/sec;
-        return Number(dogAge.toFixed(2))
-    }else if (planet=="mercury"){
-        let dogAge = (age*0.2408467*7)/sec;
-        return Number(dogAge.toFixed(2))
-    }else if (planet=="venus"){
-         let dogAge = (age*0.61519726*7)/sec;
-         return Number(dogAge.toFixed(2))
-    }else if (planet=="mars"){
-        let dogAge = (age*1.8808158*7)/sec;
-        return Number(dogAge.toFixed(2))
-    }else if (planet=="jupiter"){
-        let dogAge= (age*11.862615*7)/sec;
-        return Number(dogAge.toFixed(2))
-    }else if (planet=="saturn"){
-        let dogAge = (age*29.447498*7)/sec;
-        return Number(dogAge.toFixed(2))
-    }else if (planet=="uranus"){
-        let dogAge = (age*84.016846*7)/sec;
-        return Number(dogAge.toFixed(2))
-    }else if (planet=="neptune"){
-        let dogAge= (age*164.79132*7)/sec;
-        return Number(dogAge.toFixed(2))
-    }
+function dogYears(planetName,dogAge){
+    const Planets={
+        'earth':1,
+        'mercury':0.2408467,
+        'venus':0.61519726,
+        'mars':1.8808158,
+        'jupiter':11.862615,
+        'saturn':29.447498,
+        'uranus':84.016846,
+        'neptune':164.79132,
+    };
+    const sec=31557600;
+let year=dogAge/sec;
+let newYear=year/Planets[planetName];
+let newDogYear=newYear*7;
+return Number(newDogYear.toFixed(2));
 }

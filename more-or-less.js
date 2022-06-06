2e58a5138fad52e8ffba350404deb275243790c8
gymlad <abdalraheemkhan@icloud.com>
2022-06-06 13:22:26 +0100

    take1

---
+++ more-or-less.js
@@ -0,0 +1,19 @@
+function more(add){
+    add += 1 
+    return add 
+}
+
+function less(less){
+    less -= 1
+    return less
+}
+
+function add(one,two){
+    one += two
+    return one
+}
+
+function sub(one,two){
+    two -= one 
+    return two 
+}

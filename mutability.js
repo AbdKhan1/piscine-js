a4f426b62b998294e46dea467d91b34800ec40dc
gymlad <abdalraheemkhan@icloud.com>
2022-06-06 13:02:36 +0100

    take2

--- mutability.js
+++ mutability.js
@@ -1,8 +1,6 @@
-const clone1 = structuredClone(person);
-Object.freeze(clone1.age, clone1.name, clone1.country)
-const clone2 = structuredClone(person);
-Object.freeze(clone2.age, clone2.name, clone2.country)
-const samePerson = structuredClone(person);
+const clone1 = person;
+const clone2 = person;
+const samePerson = Oject.assign({}, person);
 
-person.age + 1 
+person.age += 1
 person.country = 'FR'

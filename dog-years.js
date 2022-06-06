5f031063580e2739212f8b5600145795049cdecb
gymlad <abdalraheemkhan@icloud.com>
2022-06-06 15:11:20 +0100

    take2

--- dog-years.js
+++ dog-years.js
@@ -1,19 +1,20 @@
 function dogYears(planet,age){
+    const sec = 31557600 
     if (planet == "earth"){
-        return age 
+        return age; 
     }else if (planet=="mercury"){
-        return age*0.2408467
+        return (age*0.2408467)/sec;
     }else if (planet=="venus"){
-        return age*0.61519726
+        return (age*0.61519726)/sec;
     }else if (planet=="mars"){
-        return age*1.8808158
+        return (age*1.8808158)/sec;
     }else if (planet=="jupiter"){
-        return age*11.862615
+        return (age*11.862615)/sec;
     }else if (planet=="saturn"){
-        return age*29.447498
+        return (age*29.447498)/sec;
     }else if (planet=="uranus"){
-        return age*84.016846
+        return (age*84.016846)/sec;
     }else if (planet=="neptune"){
-        return age*164.79132
+        return (age*164.79132)/sec;
     }
 }

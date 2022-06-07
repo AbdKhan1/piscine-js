5b398d530268cf44b55e3d92dcc0e655fe8a9ecc
gymlad <abdalraheemkhan@icloud.com>
2022-06-07 20:42:52 +0100

    take 3

--- keep-cut.js
+++ keep-cut.js
@@ -1,45 +1,49 @@
-function cutFirst(str){
+function cutFirst(str) {
     let newStr = ""
-    for (let i=2; i<str.length;i++){
-        newStr +=str[i] 
-    }
-    return newStr
-}
-
-function cutLast(str){
-    let newStr = ""
-    for (let i=0; i < str.length-2; i++){
+    for (let i = 2; i < str.length; i++) {
         newStr += str[i]
     }
     return newStr
 }
-console.log(cutLast('abcdef'))
-function cutFirstLast(str){
-    let cut1 = cutFirst(str)
-    let cut2 = cutLast(str)
-    let newStr = cut1 + cut2 
-    return newStr
-}
 
-function KeepFirst(str){
+function cutLast(str) {
     let newStr = ""
-    for (let i=0;i<2;i++){
-        newStr = str[i]
+    for (let i = 0; i < str.length - 2; i++) {
+        newStr += str[i]
     }
     return newStr
 }
 
-function KeepLast(str){
+function cutFirstLast(str) {
     let newStr = ""
-    for (let i=str.length;i>str.length-2;i--){
-        newStr = str[i]
+    for (let i = 2; i < str.length - 2; i++) {
+        newStr += str[i]
     }
     return newStr
 }
 
-function KeepFirstLast(str){
+function KeepFirst(str) {
+    let newStr = ""
+    for (let i = 0; i <=2; i++) {
+        newStr += str[i]
+    }
+    return newStr
+}
+
+function KeepLast(str) {
+    let newStr = ""
+    length = str.length-2
+    for (let i = length; i <= str.length; i++) {
+        newStr += str[i]
+    }
+    return newStr
+}
+
+function KeepFirstLast(str) {
+    let newStr =""
     let cut1 = KeepFirst(str)
     let cut2 = KeepLast(str)
-    let newStr = cut1 + cut2 
+    newStr += cut1 + cut2
     return newStr
 }
+console.log(KeepFirstLast('abdef'))

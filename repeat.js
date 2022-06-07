1754e4688dd808102ccaba5b5b7c87b85aba9047
gymlad <abdalraheemkhan@icloud.com>
2022-06-07 13:16:34 +0100

    take 4

--- repeat.js
+++ repeat.js
@@ -1,7 +1,8 @@
 function repeat(str,num){
     let newStr
-    for (let i = 0; i <= num; i++){
+    for (let i = 1; i <= num; i++){
         newStr += str 
     } 
     return newStr
 }
+console.log(repeat("test",3))

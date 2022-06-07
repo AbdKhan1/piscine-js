f4104ec9ed4bf8eca900186dd1f3f979a1a1a562
gymlad <abdalraheemkhan@icloud.com>
2022-06-07 19:50:14 +0100

    take 2

--- slicer.js
+++ slicer.js
@@ -1,17 +1,18 @@
 function slice(arr, start, end) {
-    let newArr = [""]
+    let newArr = []
     let counter = 0
-    
+
     if (start > arr.length) {
         return newArr
-    }else if (start < 0 && end > 0) {
+    } else if (start < 0 && end >= 0) {
         if (counter < end) {
-            for (let i = start + arr.length, j = 0; i < arr.length; i, j = i + 1, j + 1) {
+            for (let i = start + arr.length, j = 0; i < arr.length; i = i + 1, j = j + 1) {
                 newArr[j] = arr[i]
                 counter++
             }
         }
-    }else if ((start < 0 && end < 0)) {
+        return newArr
+    } else if ((start < 0 && end <= 0)) {
         counter = arr.length + end
         if (counter < end) {
             for (let i = arr.length + start, j = 0; i < arr.length; i = i + 1, j = j + 1) {
@@ -19,12 +20,15 @@ function slice(arr, start, end) {
                 counter++
             }
         }
-    }else if ((start >= 0 || start == undefined) && (end >= 0 && end <= arr.length)) {
+        return newArr
+    } else if ((start >= 0 || start == undefined) && (end >= 0 && end <= arr.length)) {
         if (counter < end) {
-            for (let i = start, j = 0; i < arr.length; i, j = i + 1, j + 1) {
+            for (let i = start, j = 0; i < arr.length; i = i + 1, j = j + 1) {
                 newArr[j] = arr[i]
                 counter++
             }
         }
+        return newArr
     }
 }
+console.log(slice(('abcdef', 2)))

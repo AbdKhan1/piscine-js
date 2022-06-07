d126bef6f5d8af864ba5b88cdcf6ca8e53126e25
gymlad <abdalraheemkhan@icloud.com>
2022-06-07 17:50:14 +0100

    take 1

---
+++ index-of.js
@@ -0,0 +1,33 @@
+function includes(arr, value) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == value) {
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+function lastIndexOf(arr, ele) {
+    for (let i = arr.length; i > 0; i--) {
+        if (arr[i] == ele) {
+            return i
+        }
+    }
+}
+
+function indexOf(arr, ele, index) {
+    if (index != 0) {
+        for (let i = index; i < arr.length; i++) {
+            if (arr[i] == ele) {
+                return i
+            }
+        }
+    } else {
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] == ele) {
+                return i
+            }
+        }
+    }
+}

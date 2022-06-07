52148ddcd997f16c846b3feab2f12b41d394d124
gymlad <abdalraheemkhan@icloud.com>
2022-06-07 19:41:06 +0100

    take 1

---
+++ slicer.js
@@ -0,0 +1,30 @@
+function slice(arr, start, end) {
+    let newArr = [""]
+    let counter = 0
+    
+    if (start > arr.length) {
+        return newArr
+    }else if (start < 0 && end > 0) {
+        if (counter < end) {
+            for (let i = start + arr.length, j = 0; i < arr.length; i, j = i + 1, j + 1) {
+                newArr[j] = arr[i]
+                counter++
+            }
+        }
+    }else if ((start < 0 && end < 0)) {
+        counter = arr.length + end
+        if (counter < end) {
+            for (let i = arr.length + start, j = 0; i < arr.length; i = i + 1, j = j + 1) {
+                newArr[j] = arr[i]
+                counter++
+            }
+        }
+    }else if ((start >= 0 || start == undefined) && (end >= 0 && end <= arr.length)) {
+        if (counter < end) {
+            for (let i = start, j = 0; i < arr.length; i, j = i + 1, j + 1) {
+                newArr[j] = arr[i]
+                counter++
+            }
+        }
+    }
+}

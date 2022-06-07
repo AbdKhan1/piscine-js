82d5f860da3a45f723680747a67b2a05b992d9d3
gymlad <abdalraheemkhan@icloud.com>
2022-06-07 18:32:15 +0100

    take 10

--- index-of.js
+++ index-of.js
@@ -2,10 +2,9 @@ function includes(arr, value) {
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] == value) {
             return true
-        } else {
-            return false
         }
     }
+    return false 
 }
 
 function lastIndexOf(arr, ele, index) {

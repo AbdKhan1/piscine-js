a372cf498ec14ad42d50b8b0d6444a3ded00ebfd
gymlad <abdalraheemkhan@icloud.com>
2022-06-07 20:19:39 +0100

    take 1

---
+++ keep-cut.js
@@ -0,0 +1,45 @@
+function cutFirst(str){
+    let newStr = ""
+    for (let i=2; i<str.length;i++){
+        newStr +=str[i] 
+    }
+    return newStr
+}
+
+function cutLast(str){
+    let newStr = ""
+    for (let i=0; i >= str.length-2; i--){
+        newStr += str[i]
+    }
+    return newStr
+}
+
+function cutFirstLast(str){
+    let cut1 = cutFirst(str)
+    let cut2 = cutLast(str)
+    let newStr = cut1 + cut2 
+    return newStr
+}
+
+function KeepFirst(str){
+    let newStr = ""
+    for (let i=0;i<3;i++){
+        newStr = str[i]
+    }
+    return newStr
+}
+
+function KeepLast(str){
+    let newStr = ""
+    for (let i=str.length;i>str.length-3;i--){
+        newStr = str[i]
+    }
+    return newStr
+}
+
+function KeepFirstLast(str){
+    let cut1 = KeepFirst(str)
+    let cut2 = KeepLast(str)
+    let newStr = cut1 + cut2 
+    return newStr
+}

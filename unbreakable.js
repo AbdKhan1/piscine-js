eecd8bc097c67d6b43cb5dce2c1366addec532cf
gymlad <abdalraheemkhan@icloud.com>
2022-06-08 19:37:01 +0100

    take 8

--- unbreakable.js
+++ unbreakable.js
@@ -1,10 +1,13 @@
-function split(str, splitter) {
+function split(str, splitter, num) {
     let newArr = []
     let tempStr = ""
     for (let i = 0, j = 0; i < str.length; i++) {
         if (str[i] != splitter) {
             tempStr += str[i]
         } else if (str[i] = splitter) {
+            if (num == j + 1) {
+                return newArr
+            }
             newArr[j] = tempStr
             tempStr = ""
             j++

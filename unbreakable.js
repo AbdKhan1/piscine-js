d927c956ceceedf6143b56e65140f4291731940c
gymlad <abdalraheemkhan@icloud.com>
2022-06-08 19:33:35 +0100

    take 6

--- unbreakable.js
+++ unbreakable.js
@@ -1,19 +1,24 @@
 function split(str, splitter) {
     let newArr = []
+    let tempStr = ""
     for (let i = 0, j = 0; i < str.length; i++) {
         if (str[i] != splitter) {
-            newArr[j] += str[i]
+            tempStr += str[i]
         } else if (str[i] = splitter) {
+            newArr[j] = tempStr
+            tempStr = ""
             j++
         }
     }
+    newArr[newArr.length] = tempStr
     return newArr
 }
+console.log(split('dog eats water', 'water'))
 
-function join(arr,joiner){
+function join(arr, joiner) {
     let newStr = ""
-    for(let i=0; i<arr.length;i++){
-        newStr += (arr[i]+joiner)
+    for (let i = 0; i < arr.length; i++) {
+        newStr += (arr[i] + joiner)
     }
-    return newStr 
+    return newStr
 }

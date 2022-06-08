368134379fedc75753b1e3ff66af387b92e83f9b
gymlad <abdalraheemkhan@icloud.com>
2022-06-08 18:26:00 +0100

    take 2

--- reverser.js
+++ reverser.js
@@ -1,6 +1,7 @@
-function reverse(arr){
-for(let i=0,j=arr.length; i<arr.length;i=i+1,j=j-1){
-    arr[i] = arr[j]
-}
-return arr
+function reverse(arr) {
+    arr = Array.isArray(arr)
+    for (let i = 0, j = arr.length; i < arr.length; i = i + 1, j = j - 1) {
+        arr[i] = arr[j]
+    }
+    return arr
 }

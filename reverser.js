c342b753b42ae955a279b077395be67b8557ef01
gymlad <abdalraheemkhan@icloud.com>
2022-06-08 18:32:59 +0100

    take 2

--- reverser.js
+++ reverser.js
@@ -1,7 +1,17 @@
-function reverse(arr) {
-    arr = Array.isArray(arr)
-    for (let i = 0, j = arr.length; i < arr.length; i = i + 1, j = j - 1) {
-        arr[i] = arr[j]
+function reverse(collection){
+    if (Array.isArray(collection)){
+        let tempArray=[];
+        for(let i=collection.length-1;i>=0;i--){
+        tempArray.push(collection[i]);
+        }
+        return tempArray;
+    }else if (typeof collection==="string"){
+        let tempStr="";
+        for(let i=collection.length-1;i>=0;i--){
+            tempStr+=collection[i];
+        }
+        return tempStr;
+    }else{
+        return collection;
     }
-    return arr
 }

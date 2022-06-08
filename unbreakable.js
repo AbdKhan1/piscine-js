b8dd796845e140c873ef0609424ee4d52b9630e7
gymlad <abdalraheemkhan@icloud.com>
2022-06-08 20:17:00 +0100

    take 11

--- unbreakable.js
+++ unbreakable.js
@@ -1,21 +1,54 @@
-function split(str, splitter, num) {
-    let newArr = []
-    let tempStr = ""
-    for (let i = 0, j = 0; i < str.length; i++) {
-        if (str[i] != splitter) {
-            tempStr += str[i]
-        } else if (str[i] === splitter) {
-            if (num == j + 1) {
-                return newArr
-            }
-            newArr[j] = tempStr
-            tempStr = ""
-            j++
-        }
+function split(str, splitter) {
+    let tempArray = [];
+    let tempStr = "";
+    let checkStr = "";
+    if (splitter == '') {
+      for (let i = 0; i < str.length; i++) {
+        tempStr += str[i];
+        tempArray.push(str[i]);
+        tempStr = "";
+      }
+      return tempArray;
     }
-    newArr[newArr.length] = tempStr
-    return newArr
+    if (splitter == undefined||str=='') {
+      tempArray.push(str);
+      return tempArray;
+    }
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] == splitter[0] && i < str.length - 1) {
+        for (let j = i; j < i + splitter.length; j++) {
+          checkStr += str[j]
+        }
+        if (checkStr != splitter) {
+          tempStr += str[i];
+          checkStr = ""
+        } else {
+          tempArray.push(tempStr);
+          tempStr = "";
+          checkStr = "";
+          i = i + (splitter.length - 1);
+          if (i >= str.length - 1) {
+            tempArray.push(tempStr);
+            return tempArray;
+          }
+        }
+      } else if (i == str.length - 1) {
+        if (str[i] == splitter) {
+          tempArray.push(tempStr);
+          tempArray.push('')
+          return tempArray;
+        } else {
+          tempStr += str[i];
+          tempArray.push(tempStr);
+          tempStr = "";
+        }
+      } else {
+        tempStr += str[i]
+      }
+    }
+    return tempArray;
 }
+
 
 function join(arr, joiner) {
     let newStr = ""

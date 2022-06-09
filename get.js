ef522427dd22977390eb9878000c4b4026ad490f
gymlad <abdalraheemkhan@icloud.com>
2022-06-09 15:29:12 +0100

    take 6

--- get.js
+++ get.js
@@ -2,6 +2,9 @@ function get(src, path) {
     let arr = path.split(".")
     for (let i = 0; i < arr.length; i++) {
         src = src[arr[i]]
+        if (src === undefined) {
+            return undefined
+        }
     }
     return src
 }

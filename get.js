af66db011a716849d66325d3cf17975c86520141
gymlad <abdalraheemkhan@icloud.com>
2022-06-09 15:21:59 +0100

    take 5

--- get.js
+++ get.js
@@ -1,12 +1,7 @@
 function get(src, path) {
-    let value = undefined
     let arr = [path.split(".")]
     for (let i = 0; i < arr.length; i++) {
-        value = src[arr[i]]
-        if (src[arr[i]] == undefined) {
-            return undefined
-        }
+        src = src[arr[i]]
     }
-    return value
+    return src
 }
-console.log(get({ nested: { key: 'value' } }, 'nested.key') === 'value')

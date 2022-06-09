b2f384849660e5f10c78db6615a5da5de4bf493f
gymlad <abdalraheemkhan@icloud.com>
2022-06-09 13:44:39 +0100

    take 2

--- nasa.js
+++ nasa.js
@@ -1,8 +1,10 @@
-function chunk(arr, chunkSize) {
-    if (chunkSize <= 0) throw "Invalid chunk size";
-    var result = [];
-    for (let i=0,len=arr.length; i<len; i+=chunkSize)
-    result.push(arr.slice(i,i+chunkSize));
-    return result;
-  }
-  
+function nasa(N) {
+    let res = ""
+    for (let i = 1; i <= N; i++) {
+        if (i % 3 === 0) res += "NA"
+        if (i % 5 === 0) res += "SA"
+        else if (!(i % 3 === 0)) res += i
+        if (i !== N) res += " "
+    }
+    return res
+}

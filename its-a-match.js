eaca3f00041054db7c80d7d4450ca3abd01aa4ae
gymlad <abdalraheemkhan@icloud.com>
2022-06-10 22:32:11 +0100

    take 4

--- its-a-match.js
+++ its-a-match.js
@@ -1,4 +1,4 @@
-const normal = new RegExp('\\bhi\\b', 'gi');
-const begin = new RegExp('\\b^(hi)\\b', 'gi');
-const end = new RegExp('\\b$(hi)\\b', 'gi');
-const beginEnd = new RegExp('\\b$^(hi)\\b', 'gi');
+const normal = new RegExp('\\bhi\\b');
+const begin = new RegExp('\\b^(hi)\\b');
+const end = new RegExp('\\b$(hi)\\b');
+const beginEnd = new RegExp('\\b$^(hi)\\b');

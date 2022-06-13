98133b1b41f44f305c01c4e9a0fa87d0b1b008f9
gymlad <abdalraheemkhan@icloud.com>
2022-06-13 17:56:12 +0100

    take 2

--- date-is.js
+++ date-is.js
@@ -1,18 +1,8 @@
 function isValid(date) {
-    if (date instanceof Date) {
-        let timeInMilliSecond = date.valueOf();
-        let isInvalidDate = isNaN(timeInMilliSecond);
-        if (isInvalidDate) {
-            return false; // if timeInMilliSecond = NaN means it is an invalid date
-        }
-        return true; // valid date value
-    }
-    return false; // obj is not an Date object
+    return date >0 && date !=null
 }
 
 function isAfter(date1, date2) {
-    var date1 = new Date();
-    var date2 = new Date();
     if (date1.getTime() > date2.getTime())
         return true;
     else {
@@ -21,8 +11,6 @@ function isAfter(date1, date2) {
 }
 
 function isBefore(date1, date2) {
-    var date1 = new Date();
-    var date2 = new Date();
     if (date1.getTime() < date2.getTime())
         return true;
     else {
@@ -31,19 +19,11 @@ function isBefore(date1, date2) {
 }
 
 function isFuture(date) {
-    if (isValid(date) == true) {
-        date1 = new Date.now();
-        return (isAfter(date, date1));
-    } else {
-        return false;
-    }
+    date1 = new Date.now();
+    return isValid(date) && (isAfter(date, date1));
 }
 
 function isPast(date) {
-    if (isValid(date) == true) {
-        date1 = new Date.now();
-        return (isBefore(date, date1));
-    } else {
-        return false;
-    }
+    date1 = new Date.now();
+    return isValid(date) && (isBefore(date, date1));
 }

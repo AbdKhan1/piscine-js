48ee7e858177ad318db0bec589b3a5bda4ba1c28
gymlad <abdalraheemkhan@icloud.com>
2022-06-13 19:09:07 +0100

    take 13

--- date-is.js
+++ date-is.js
@@ -1,5 +1,5 @@
 function isValid(date) {
-    return (date === 'Invalid Date' && !isNaN(date) && date !== '')
+    return (new Date(date) !== "Invalid Date") && !isNaN(new Date(date) && date !== '');
 }
 
 function isAfter(date1, date2) {

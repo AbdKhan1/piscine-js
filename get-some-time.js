c807cc3f736e5b93253f84a0457d55554917a031
gymlad <abdalraheemkhan@icloud.com>
2022-06-14 13:42:09 +0100

    take 1

---
+++ get-some-time.js
@@ -0,0 +1,7 @@
+function firstDayWeek(num, str) {
+    let year = Math.floor(str);
+    let month = Math.round(num / 4.3452381);
+    let days = (num*7)-5
+    d = new Date(year,month,days)
+    return d
+}

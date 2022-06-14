ff3062145820d86bc6aff4c412563dcda04786db
gymlad <abdalraheemkhan@icloud.com>
2022-06-14 17:55:57 +0100

    take 2

--- day-of-the-year.js
+++ day-of-the-year.js
@@ -2,7 +2,7 @@ function dayOfTheYear(date) {
     let d = new Date(date)
     d.setMonth(0)
     d.setDate(0)
-    diffTime = Math.abs(d - date)
-    days = Math.ceil(diffTime / (1000 * 60 * 60 * 24))
+    let diffTime = Math.abs(d - date)
+    let days = Math.ceil(diffTime / (1000 * 60 * 60 * 24))
     return days
 }
